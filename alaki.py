eea7f25e21a43dbbf4ac7efbf348cc5a5129393b
megacodist <megacodist@gmail.com>
2024-03-24 17:05:20 +0430

    Developing 03-01-05

--- alaki.py
+++ alaki.py
@@ -1,5 +1,17 @@
+from abc import abstractmethod
 import logging
 import os
+from typing import TypeVar
+
+
+_T = TypeVar('_T')
+class Foo:
+    def __init__(self) -> None:
+        self._items: dict[int, _T] = {}
+
+    @abstractmethod
+    def Bar(self, __id: int, /) -> _T:
+        pass
 
 
 def foo(bar: list = []) -> None:
@@ -9,16 +21,9 @@ def foo(bar: list = []) -> None:
     print('-' * 10)
  
 if __name__ == '__main__':
-    logger = logging.getLogger()
-    fileHandler = logging.FileHandler(
-        filename=r'd:\bjbd',
-        mode='a')
-    fileHandler.setLevel(logging.INFO)
-    consoleHandler = logging.StreamHandler()
-    baz = []
-    foo()
-    foo()
-    foo()
-    foo(baz)
-    foo()
-    print(baz)
+    import tomllib
+    with open(
+            r'F:\Mohsen\Programming\Python\happy-eng-bot\config.toml',
+            mode='rb') as fileObj:
+        config = tomllib.load(fileObj)
+    print(config)
